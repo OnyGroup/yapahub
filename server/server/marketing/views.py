ce441624730f563778f7d8f9a00ff487ef4220c3
Murithi <kelvinmurithi@live.com>
2025-02-12 20:28:03 +0300

    Added 'POST' Method to DiscountCodeListView

--- server/server/marketing/views.py
+++ server/server/marketing/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Campaign, CampaignPerformance, DiscountCode
 from .serializers import CampaignSerializer, CampaignPerformanceSerializer, DiscountCodeSerializer
+from rest_framework import status
 
 class CampaignListView(APIView):
     permission_classes = [IsAuthenticated]
@@ -37,3 +38,10 @@ class DiscountCodeListView(APIView):
         codes = DiscountCode.objects.all()
         serializer = DiscountCodeSerializer(codes, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DiscountCodeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
