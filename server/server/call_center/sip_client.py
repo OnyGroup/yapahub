6915ec23435377644f164a564c7c21aa778889b0
Kelvin <kelvinmurithi@live.com>
2025-03-20 13:16:55 +0300

    Solved settings import errors in sip_client.py

--- server/server/call_center/sip_client.py
+++ server/server/call_center/sip_client.py
@@ -1,9 +1,10 @@
 import pjsua2 as pj
 import os
-from decouple import config
+# from decouple import config
+from django.conf import settings
 
-SIP_USERNAME = config('SIP_USERNAME')
-SIP_PASSWORD = config('SIP_PASSWORD')
+# SIP_USERNAME = config('SIP_USERNAME')
+# SIP_PASSWORD = config('SIP_PASSWORD')
 SIP_DOMAIN = "ke.sip.africastalking.com"
 
 class MyAccount(pj.Account):
@@ -28,9 +29,12 @@ class SIPClient:
 
         # Account configuration
         acfg = pj.AccountConfig()
-        acfg.idUri = f"sip:{SIP_USERNAME}@{SIP_DOMAIN}"
-        acfg.regConfig.registrarUri = f"sip:{SIP_DOMAIN}"
-        acfg.sipConfig.authCreds.append(pj.AuthCredInfo("digest", "*", SIP_USERNAME, 0, SIP_PASSWORD))
+        # acfg.idUri = f"sip:{SIP_USERNAME}@{SIP_DOMAIN}"
+        # acfg.regConfig.registrarUri = f"sip:{SIP_DOMAIN}"
+        # acfg.sipConfig.authCreds.append(pj.AuthCredInfo("digest", "*", SIP_USERNAME, 0, SIP_PASSWORD))
+        acfg.idUri = f"sip:{settings.SIP_USERNAME}@ke.sip.africastalking.com"
+        acfg.regConfig.registrarUri = f"sip:ke.sip.africastalking.com"
+        acfg.sipConfig.authCreds.append(pj.AuthCredInfo("digest", "*", settings.SIP_USERNAME, 0, settings.SIP_PASSWORD))
 
         self.account = MyAccount()
         self.account.create(acfg)
@@ -54,7 +58,7 @@ class SIPClient:
         # add a small delay to ensure the call starts
         # before Django destroys the thread
         import time
-        time.sleep(1)
+        time.sleep(5)
 
 if __name__ == "__main__":
     client = SIPClient()
