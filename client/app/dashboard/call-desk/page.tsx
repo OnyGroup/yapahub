04f0ad9d6e3ee94898c7c8271b3a74076def9e22
Kelvin <kelvinmurithi@live.com>
2025-03-21 16:53:56 +0300

    Can now initiate calls from the frontend

---
+++ client/app/dashboard/call-desk/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CallManager from './CallManager';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <CallManager />
+    </div>
+  );
+};
+
+export default HomePage;
